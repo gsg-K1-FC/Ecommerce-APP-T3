106b7649f4c7844eb60fcaac6d4faffd5b64701a
sondos zaro <197075@ppu.edu.ps>
2020-11-24 23:20:57 +0200

    edit seller page style and add header

--- Assets/js/seller-script.js
+++ Assets/js/seller-script.js
@@ -4,14 +4,24 @@ let price = document.getElementById("input-price");
 let image = document.getElementById("input-image");
 let category = document.getElementById("input-category");
 let allProducts = document.querySelector(".allProducts");
+let Containers = document.querySelector(".Containers");
 let storedProduct = JSON.parse(localStorage.getItem("productForm"));
 let addProducts = document.getElementById("add");
 let submitForm = document.querySelector(".seller-form");
 let popUpForm = document.querySelector('.drop-down-form');
+let addStatement = document.querySelector(".addStatement");
 let products = storedProduct ? storedProduct : [];
 let productForm = {};
 let openForm = false;
 let openEditProduct = false;
+
+
+let addPara = document.createElement("p");
+addPara.className = "add-p"
+addPara.innerText = "you don't have any product if you want to add product click on Add product";
+
+
+
 
 
 
@@ -31,12 +41,21 @@ addProducts.addEventListener('click', function() {
 
 // show the new product on the screen
 function showProduct() {
+
     allProducts.innerHTML = "";
+    if (products.length == 0) {
+
+        addStatement.style.display = "flex"
+        addStatement.appendChild(addPara)
+
+    } else {
+        addStatement.style.display = "none"
+    }
     products.map(function(product, index) {
 
         let nameLabel = document.createElement("span");
         let priceLabel = document.createElement("span");
-        let detailsLabel = document.createElement("span");
+
         let categoryLabel = document.createElement("span");
         let Image = document.createElement("img");
         let productContainer = document.createElement("div");
@@ -44,11 +63,16 @@ function showProduct() {
         let sellerBtn = document.createElement("div");
         let deleteBtn = document.createElement("button");
         let editBtn = document.createElement("button");
+        let pContainer = document.createElement("div");
 
-
+        allProducts.appendChild(productContainer);
         productContainer.className = "product";
         data.className = "data";
-        nameLabel.className = "name";
+        pContainer.className = "container"
+        nameLabel.className = "product-content-span";
+        Image.className = "product-img";
+        priceLabel.className = "product-content-span"
+        categoryLabel.className = "product-category-div";
         deleteBtn.innerText = "Delete";
         editBtn.innerText = "Edit";
         editBtn.className = "edit-btn"
@@ -56,23 +80,26 @@ function showProduct() {
         deleteBtn.className = "delete-btn"
         editBtn.className = "edit-btn"
 
-
+        // console.log(product, 'llllllll')
         nameLabel.innerText = product.productName;
         priceLabel.innerText = product.productPrice;
-        detailsLabel.innerText = product.productDetails;
-        categoryLabel.innerText = product.productCategory;
+
+        categoryLabel.innerText = "Category:" + product.productCategory;
         Image.src = product.productImg;
 
 
         allProducts.appendChild(productContainer);
 
         productContainer.appendChild(Image);
-        productContainer.appendChild(nameLabel);
-        productContainer.appendChild(data);
-        productContainer.appendChild(sellerBtn);
-        data.appendChild(priceLabel);
-        data.appendChild(detailsLabel);
-        data.appendChild(categoryLabel);
+
+        productContainer.appendChild(pContainer);
+        // pContainer.appendChild(data); 
+        pContainer.appendChild(nameLabel);
+        pContainer.appendChild(priceLabel);
+        pContainer.appendChild(categoryLabel)
+        pContainer.appendChild(sellerBtn);
+
+
         sellerBtn.appendChild(deleteBtn);
         sellerBtn.appendChild(editBtn);
 
@@ -83,7 +110,10 @@ function showProduct() {
             products.splice(index, 1);
             showProduct();
 
+
         });
+
+
 
         // edit the product name and price
         editBtn.addEventListener('click', function() {
@@ -95,18 +125,19 @@ function showProduct() {
             let submitEdit = document.createElement("button");
             let editDiv = document.createElement('div')
             submitEdit.innerText = "submit";
+            editDiv.className = "edit-div"
             nameInput.value = product.productName;
             priceInput.value = product.productPrice;
             editName.innerText = "Edit Name :";
             editPrice.innerText = "Edit Price :";
-            submitEdit.id = "submit-form"
+            submitEdit.className = "editSubmit"
             if (!openEditProduct) {
                 editDiv.appendChild(editName);
                 editDiv.appendChild(nameInput);
                 editDiv.appendChild(editPrice);
                 editDiv.appendChild(priceInput);
                 editDiv.appendChild(submitEdit);
-                productContainer.appendChild(editDiv);
+                pContainer.appendChild(editDiv);
                 openEditProduct = true;
             }
 
@@ -143,7 +174,9 @@ submitForm.addEventListener('submit', function(event) {
     products.push(productForm);
     popUpForm.style.display = "none";
     localStorage.setItem("productForm", JSON.stringify(products));
+
     showProduct();
+
     openForm = false;
     name.value = "";
     detail.value = "";
